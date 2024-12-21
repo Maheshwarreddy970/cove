bf9f93a9af4acaa0758b4c3492d2a4cf45ebfde2
Maheshwarreddy970 <mr8382455@gmail.com>
2024-12-22 00:09:25 +0530

    adding

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -13,17 +13,17 @@ const adventpro = Advent_Pro({ weight: '700', subsets: ["latin"] });
 export default function Overview() {
   const [activeImage, setActiveImage] = useState(0)
 
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setActiveImage((prevActiveImage) => {
-  //       return (prevActiveImage + 1) % 3;
-  //     });
-  //   }, 9000)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveImage((prevActiveImage) => {
+        return (prevActiveImage + 1) % 3;
+      });
+    }, 9000)
 
-  //   return () => {
-  //     clearInterval(intervalId)
-  //   }
-  // }, [])
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
 
   return (
     <section className={` bg-[#232526] text-white py-24 lg:px-5 overflow-hidden`}>
